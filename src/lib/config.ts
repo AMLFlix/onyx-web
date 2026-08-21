2f905fd252b41635a5726c5b1091347326ce541f
AMLFlix <111834180+AMLFlix@users.noreply.github.com>
2026-08-21 09:17:00 +0630

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 // src/lib/config.ts
-export const PLAYER_DOMAIN = 'https://stream-nexusmm.xyz';
+export const PLAYER_DOMAIN = 'https://play.nexusmm.xyz';
 export const APP_DOWNLOAD_URL = 'https://your-download-link.com/app.apk'; // သင်၏ App Download Link ထည့်ရန်
