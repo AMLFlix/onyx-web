f80fd3cfa64bc33d9b70cf0a0ec549495664c956
AMLFlix <aungmyolinn5584@gmail.com>
2026-08-22 20:01:45 +0630

    app download link update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 export const PLAYER_DOMAIN = 'https://play.nexusmm.xyz';
-export const APP_DOWNLOAD_URL = 'https://your-download-link.com/app.apk';
+export const APP_DOWNLOAD_URL = 'https://huggingface.co/datasets/ryannyo24/onyx-app-releases/resolve/main/app-release.apk';
